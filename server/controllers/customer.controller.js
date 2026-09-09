72c774298dd05ec03273272d81f11d21b3e5c137
Ran1t <ranitlaha00@gmail.com>
2026-09-09 18:43:41 +0530

    Implement password hashing, JWT authentication, and cookie-based token storage

--- server/controllers/customer.controller.js
+++ server/controllers/customer.controller.js
@@ -1,4 +1,11 @@
 import { Customer } from "../models/customer.model.js"
+import bcrypt from "bcrypt"
+import { genToken } from "../utils/generateTokens.js"
+
+const cookiesOptions={
+    httpOnly : true,
+    secure : true
+}
 
 export const registerCustomer= async (req,res)=>{
     try{
@@ -16,16 +23,29 @@ export const registerCustomer= async (req,res)=>{
         if(emailExists){
             return res.status(409).json({message:"Email already exists"})
         }
+
+        // password security
+
+        const salt=await bcrypt.genSalt(10)
+        const hashedPassword=await bcrypt.hash(password,salt)
+
         const newCustomer=await Customer.create({
             fullname,
             email,
-            password,
+            password : hashedPassword,
             phone
         }) 
-        res.status(201).json({message:"Customer Registered Successfully"})
+
+        // jwt token
+
+        const token=genToken(newCustomer._id)
+
+        res.cookie("token",token,cookiesOptions)
+
+        res.status(201).json({success:true ,message:"Customer Registered Successfully",customer:newCustomer})
     }
     catch(error){
-        res.status(500).json({message:"Internal Serevr Error",error:error})
+        res.status(500).json({message:"Internal Server Error",error:error})
     }
 }
 
@@ -38,7 +58,18 @@ export const loginCustomer=async (req,res)=>{
             return res.status(404).json({message:"Invalid Credentials"})
         }
         
-        res.status(200).json({message:"Login Successfully"})
+        const passwordCheck=await bcrypt.compare(password,customer.password)
+
+        if(!passwordCheck){
+            return res.status(400).json({message:"Wrong Password"})
+        }
+
+        // jwt token 
+
+        const token=genToken(customer._id)
+        res.cookie("token",token,cookiesOptions)
+
+        res.status(200).json({success:true, message:"Login Successfully"})
     }
     catch(error){
         res.status(500).json({message:"Internal Server Error",error:error})
